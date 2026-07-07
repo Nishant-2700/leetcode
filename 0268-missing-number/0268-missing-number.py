b8b3f40c67b8732e53a355ba6be766556a2e3771
Nishant-2700 <guptanishant092@gmail.com>
2026-07-07 18:26:53 +0530

    Time: 0 ms (100%), Space: 20.6 MB (18.44%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,13 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
         n = len(nums)
-        total = sum(nums)
-        lsum = 0
 
         expected = n * (n + 1) // 2
-        
-        return expected-total
+        actual = sum(nums)
+        return expected-actual
 
 
         
