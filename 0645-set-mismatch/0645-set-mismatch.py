23cbe46d3297bec0b7e110733abb5129fa2ae796
Nishant-2700 <guptanishant092@gmail.com>
2026-07-11 21:18:11 +0530

    Time: 11 ms (67.03%), Space: 21.1 MB (28.19%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        seen = set()
+        
+        for num in nums:
+            if num in seen:
+                duplicate = num
+            else:
+                seen.add(num)
+
+
+        for i in range(1,len(nums)+1):
+            if i not in seen:
+                missing = i
+        
+        return [duplicate,missing]
