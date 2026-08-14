aa812d6c0d8446d1dc938c2ca062999ed248b2cb
Nishant-2700 <guptanishant092@gmail.com>
2026-08-14 15:51:21 +0530

    Time: 159 ms (99.04%), Space: 30.4 MB (99.34%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+        
+        
+        is_prime = bytearray(b'\x01') * n
+        is_prime[0:2] = b'\x00\x00'
+        
+        for i in range(2, int(n ** 0.5) + 1):
+            if is_prime[i]:
+                is_prime[i*i:n:i] = b'\x00' * ((n - 1 - i*i) // i + 1)
+        
+        return sum(is_prime)
